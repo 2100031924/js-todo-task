3be7f7c1e5564f74430c9e6db48a86a17781131c
2100031924 <2100031924@kluniversity.in>
2026-02-17 15:24:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// Select input, button and list
+let input = document.querySelector("#taskInput");
+let button = document.querySelector("#addBtn");
+let ol = document.querySelector("#taskList");
+
+// Run when button is clicked
+button.addEventListener("click", () => {
+    const taskValue = input.value;
+
+    // Stop if input is empty
+    if (taskValue.trim() === "") {
+        alert("Please type a task!");
+        return;
+    }
+
+    // Create new list item
+    let li = document.createElement("li");
+
+    // Add text and style
+    li.innerText = taskValue;
+    li.style.listStylePosition = "inside";
+    li.setAttribute("class", "active");
+
+    // Add item to list
+    ol.append(li);
+
+    // Remove item on click
+    li.addEventListener("click", () => {
+        li.remove();
+    });
+
+    // Clear input box
+    input.value = "";
+    input.focus();
+});
